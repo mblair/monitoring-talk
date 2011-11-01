30112ae6be4c243bed3fb1c122a9d96bc9a79910
Matt Blair <me@matthewblair.net>
2011-11-01 10:27:25 -0700

    new webfonts js

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
-$('content').prepend('<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel='stylesheet' type='text/css'>');
+//Old:
+//$('content').prepend('<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel='stylesheet' type='text/css'>');
+WebFontConfig = {
+	google: { families: [ 'Ubuntu::latin' ] }
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
